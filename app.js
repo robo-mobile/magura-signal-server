1a0e37caf3a8c2c625289ad34c09463eef588948
unknown <rostanets@gmail.com>
2023-07-30 13:21:13 +0300

    upd

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const indexRouter = require('./routes/index');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
