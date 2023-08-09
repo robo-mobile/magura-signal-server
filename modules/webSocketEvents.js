12b5ab6f9a05ebca412e4a525494b187a3fcc76d
unknown <rostanets@gmail.com>
2023-08-09 22:37:35 +0300

    upd

--- modules/webSocketEvents.js
+++ modules/webSocketEvents.js
@@ -16,7 +16,9 @@ const webSocketEvents = (ws, req) => {
   ws.onclose = closeHandler;
   ws.onmessage = messageHandler;
   ws.onbind = bindHandler;
-  ws.offer = offerHandler;
+  ws.onoffer = offerHandler;
+  ws.onanswer = answerHandler;
+  ws.onice = iceHandler;
   ws.emitEvent = (eventName, payload) => {
     const mess = {
       type: eventName,
@@ -25,10 +27,10 @@ const webSocketEvents = (ws, req) => {
     console.log('mess: ', mess);
     ws.send(JSON.stringify(mess));
   }
-  ws.broadcast = function (room, event) {
+  ws.broadcast = function (room, eventName, event) {
     const couples = clients
-      .filter(client => client !== ws && client.room === room)
-    couples.forEach(client => client.send(JSON.stringify(event)))
+      .filter(client => client !== ws && client.room === room);
+    couples.forEach(client => client.emitEvent(eventName, event))
   }
 };
 
@@ -37,10 +39,8 @@ const closeHandler = ws => {
 };
 
 const messageHandler = event => {
-  console.log('message: ', event);
   const data = JSON.parse(event.data);
   const socket = event.target;
-  console.log('data: ', data);
   const handlerName = `on${data.type}`;
   if (socket[handlerName]) {
     socket[handlerName](data.payload);
@@ -48,13 +48,14 @@ const messageHandler = event => {
 };
 
 const bindHandler = function (event) {
-  console.log('BIND')
+  console.log('BIND: ', event)
   const { receiverUUID } = event;
   const hasReceiver = clients.some(client => client.room === receiverUUID);
 
   if (hasReceiver) {
     this.room = receiverUUID;
     this.emitEvent('binded', event);
+    console.log('rooms: ', clients.map(i => i.room))
   } else {
     this.emitEvent('error', {
       message: 'receiver not found'
@@ -63,8 +64,16 @@ const bindHandler = function (event) {
 };
 
 const offerHandler = function (event) {
-  this.broadcast(event.room, event);
+  console.log('event: ', this.room, 'offer', event);
+  this.broadcast(this.room, 'offer', event);
 };
 
+const answerHandler = function (event) {
+  this.broadcast(this.room, 'answer', event);
+};
+
+const iceHandler = function (event) {
+  this.broadcast(this.room, 'ice', event);
+};
 
 module.exports = webSocketEvents
